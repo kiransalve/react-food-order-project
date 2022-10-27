b4c5786e865c1903aba3617fd49f307e58213b8f
kiransalve <salvekiran2011@gmail.com>
2022-10-27 12:23:18 +0530

    meal component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from "./component/Layout/Header";
+import Meal from "./component/Meal/Meal";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Meal />
+      </main>
     </>
   );
 }
---
+++ src/component/Meal/Meal.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MealsSummary from './MealSummary';
+import AvailableMeals from './AvailableMeals'
+
+const Meal = () => {
+    return (
+        <>
+            <MealsSummary />
+            <AvailableMeals />
+        </>
+    )
+}
+
+export default Meal
