472579693ecf8f7bfdf34b25804db40fac65257f
kiransalve <salvekiran2011@gmail.com>
2022-11-06 10:36:20 +0530

    modal added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Cart from "./component/Cart/Cart";
 import Header from "./component/Layout/Header";
 import Meal from "./component/Meal/Meal";
 
 function App() {
   return (
     <>
+    <Cart />
       <Header />
       <main>
         <Meal />
